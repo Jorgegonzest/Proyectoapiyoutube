0c5a61b28fbd622e0792a1260ce8000aaacd28f0
Jorgegonzest <jordialaquas@gmail.com>
2018-02-24 13:02:47 +0100

    Initial commit

---
+++ app/src/main/java/app/com/proyectoapiyoutube/interfaces/OnItemClickListener.java
@@ -0,0 +1,13 @@
+package app.com.proyectoapiyoutube.interfaces;
+
+
+import app.com.proyectoapiyoutube.models.YoutubeDataModel;
+
+/**
+ * Created by mdmunirhossain on 12/19/17.
+ */
+
+public interface OnItemClickListener {
+    void onItemClick(YoutubeDataModel item);
+
+}
